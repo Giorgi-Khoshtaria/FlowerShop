d39ebe391b791e3d18ca567d472f3eb115ca1ddd
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2024-08-29 13:34:02 +0400

    add colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#F2F2F2",
+        black: "#000",
+        semiBlack: "#121212",
+        yellow: "#FF8F52",
+      },
+    },
   },
   plugins: [],
 };
